7e001427cc291449606c9fb756efd650d72dbea1
KalebNails <102830532+KalebNails@users.noreply.github.com>
2022-09-17 18:39:37 -0400

    Create Arduino_broadcasting.py

---
+++ Arduino_broadcasting.py
@@ -0,0 +1,5 @@
+#9/16/2022
+#Used in MOVE UAS
+
+#Python 2
+#This will broadcast 3 floats to a serial ports. To decode it from the computer decode with the cobs library, and then the serial library. 
